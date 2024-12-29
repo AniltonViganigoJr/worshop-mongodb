5aecc7f52c077f612232753b22e750915b4342a5
Anilton Vigânigo Júnior <avjrawscloudstudies@gmail.com>
2024-12-28 22:01:13 -0300

    MongoDB: connection and User: repository e service

--- src/main/java/com/example/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/example/workshopmongodb/resources/UserResource.java
@@ -1,30 +1,26 @@
 package com.example.workshopmongodb.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.workshopmongodb.domain.User;
+import com.example.workshopmongodb.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
 	public ResponseEntity<List<User>> findAll(){
-		List<User> users = new ArrayList<>();
-
-		User user1 = new User("1001", "User Number #01", "user_number1@gmail.com");
-		User user2 = new User("1002", "User Number #02", "user_number2@gmail.com");
-		User user3 = new User("1003", "User Number #03", "user_number3@gmail.com");
-
-		users.addAll(Arrays.asList(user1, user2,user3));
-		
+		List<User> users = service.findAll();		
 		return ResponseEntity.ok().body(users);
 	}
 }
